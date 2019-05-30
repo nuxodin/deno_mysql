3b034197e7015a1062d65bb344851b2d59e6de04
Tobias Buschor <tobias.buschor@shwups.ch>
2019-05-30 11:20:06 +0200

    strings/strings.ts is no more

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
 export { format as byteFormat } from "https://deno.land/x/bytes_formater/mod.ts";
-export { decode, encode } from "https://deno.land/x/std/strings/strings.ts";
+export { decode } from "https://deno.land/x/std/strings/decode.ts";
+export { encode } from "https://deno.land/x/std/strings/encode.ts";
